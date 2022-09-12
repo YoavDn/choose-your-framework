333be120397804a9b1099e64feeb8aa9dfcbd7ef
yoav <yoavdan.m@gmail.com>
2022-09-12 16:22:05 +0300

    game start

--- src/main.ts
+++ src/main.ts
@@ -14,8 +14,78 @@ bgImage.src = "./src/assets/game-map.png"
 const playerImage = new Image()
 playerImage.src = './src/assets/char.png'
 
-playerImage.onload = () => {
-    c!.drawImage(bgImage, -100, -520)
-    c?.drawImage(playerImage, (canvas.width / 2) - playerImage.width, 375)
+type positionType = { x: number, y: number }
+
+class Sprite {
+    position: positionType;
+    image: HTMLImageElement
+    constructor(pos: positionType, velocity: any, image: HTMLImageElement) {
+        this.position = pos
+        this.image = image
+    }
+
+    draw() {
+        c!.drawImage(this.image, -100, -520)
+    }
 }
 
+const background = new Sprite({ x: -100, y: -520 }, 'hi', bgImage)
+
+
+
+function animate() {
+    background.draw()
+    window.requestAnimationFrame(animate)
+    // console.log('animate');
+    c?.drawImage(playerImage, (canvas.width / 2) - playerImage.width, 375)
+}
+animate()
+
+
+const keys = {
+    left: false,
+    right: false,
+    down: false,
+    up: false,
+    space: false
+}
+
+document.addEventListener('keydown', (e) => {
+    // console.log(e.key);
+
+    switch (e.key) {
+        case 'ArrowLeft':
+            console.log('left');
+            keys.left = true
+        case "ArrowRight":
+            console.log('right');
+            keys.right = true
+        case "UpArrow":
+            keys.up = true
+        case "DownArrow":
+            keys.down = true
+        case "Space":
+            keys.space = true
+
+    }
+})
+
+document.addEventListener('keyup', (e) => {
+    // console.log(e.key);
+
+    switch (e.key) {
+        case 'ArrowLeft':
+            console.log('left');
+            keys.left = false
+        case "ArrowRight":
+            console.log('right');
+            keys.right = false
+        case "UpArrow":
+            keys.up = false
+        case "DownArrow":
+            keys.down = false
+        case "Space":
+            keys.space = false
+
+    }
+})
