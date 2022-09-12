2f35ecfdfb3baff03f3a6a576063991f9fa86b4f
yoav <yoavdan.m@gmail.com>
2022-09-12 19:09:05 +0300

    walking and stading player

--- src/main.ts
+++ src/main.ts
@@ -11,8 +11,20 @@ c?.fillRect(0, 0, canvas.width, canvas.height)
 
 const bgImage = new Image()
 bgImage.src = "./src/assets/game-map.png"
-const playerImage = new Image()
-playerImage.src = './src/assets/char.png'
+
+// player images
+const playerWalk = new Image()
+playerWalk.src = './src/assets/player-walk.png'
+
+const playerWalkLeft = new Image()
+playerWalkLeft.src = './src/assets/player-walk-left.png'
+
+const playerStand = new Image()
+playerStand.src = './src/assets/player-stand.png'
+
+const playerStandLeft = new Image()
+playerStandLeft.src = './src/assets/player-stand-left.png'
+
 
 type positionType = { x: number, y: number }
 
@@ -34,15 +46,23 @@ class Player {
     isMoving: boolean
     image: HTMLImageElement
     frames: { max: number, val: number, elapsed: number }
+    sprites: { walk: HTMLImageElement, stand: HTMLImageElement, walkLeft: HTMLImageElement, standLeft: HTMLImageElement }
+
     constructor(pos: positionType, image: HTMLImageElement, frames = { max: 1, val: 0, elapsed: 0 }) {
         this.position = pos
         this.image = image
         this.frames = frames
         this.isMoving = false
+        this.sprites = {
+            stand: playerStand,
+            standLeft: playerStandLeft,
+            walk: playerWalk,
+            walkLeft: playerWalkLeft
+        }
     }
 
     draw() {
-        c!.drawImage(
+        this.isMoving ? c!.drawImage(
             this.image,
             64 * this.frames.val,
             0,
@@ -52,7 +72,12 @@ class Player {
             this.position.y,
             this.image.width / 6,
             this.image.height
+        ) : c!.drawImage(
+            this.image,
+            this.position.x,
+            this.position.y
         )
+
         if (!this.isMoving) return
 
         if (this.frames.max > 1) {
@@ -69,8 +94,8 @@ class Player {
 
 const background = new Sprite({ x: -1000, y: -520 }, 'hi', bgImage)
 const player = new Player(
-    { x: (canvas.width / 2) - playerImage.width, y: 375 },
-    playerImage,
+    { x: (canvas.width / 2) - playerStand.width / 2, y: 375 },
+    playerStand,
     { max: 5, val: 0, elapsed: 0 })
 
 const keys = {
@@ -90,16 +115,18 @@ function animate() {
     // c?.drawImage(playerImage, (canvas.width / 2) - playerImage.width, 375)
     player.draw()
 
+
+    /// borders logic
     if (background.position.x < 0 && background.position.x > -1900) {
         if (keys.left) background.position.x += 3
         else if (keys.right) background.position.x -= 3
 
     } else {
-        if (player.position.x > canvas.width / 2 - player.image.width / 2 &&
+        if (player.position.x > canvas.width / 2 - player.sprites.stand.width / 2 &&
             !keys.left &&
             background.position.x > -500) {
             background.position.x -= 3
-        } else if (player.position.x < canvas.width / 2 - player.image.width / 2 &&
+        } else if (player.position.x < canvas.width / 2 - player.sprites.stand.width / 2 &&
             !keys.right &&
             background.position.x < -1000) {
             background.position.x += 3
@@ -107,8 +134,8 @@ function animate() {
         else if (keys.left && player.position.x > 0) player.position.x -= 3
         else if (keys.right && player.position.x < 900) player.position.x += 3
     }
-    // console.log(background.position.x);
-    // console.log(player.position.x, canvas.width / 2 - player.image.width / 2);
+    // console.log(keys);
+
 
 }
 animate()
@@ -117,34 +144,45 @@ animate()
 
 
 document.addEventListener('keydown', (e) => {
-    // console.log(e.key);
+    console.log(e.key);
 
     switch (e.key) {
         case 'ArrowLeft':
             keys.left = true
             player.isMoving = true
+            player.image = player.sprites.walkLeft
+            break
         case "ArrowRight":
-            keys.right = true
             player.isMoving = true
+            keys.right = true
+            player.image = player.sprites.walk
+            break
         case "UpArrow":
-            keys.up = true
+            keys.up = false
         case "DownArrow":
-            keys.down = true
+            keys.down = false
         case "Space":
-            keys.space = true
+            keys.space = false
 
     }
 })
 
 document.addEventListener('keyup', (e) => {
-    // console.log(e.key);
+    console.log(e.key, 'hsit');
     switch (e.key) {
         case 'ArrowLeft':
+            player.isMoving = false
             keys.left = false
-            player.isMoving = false
+            player.image = player.sprites.standLeft
+            console.log('fuck');
+            break
         case "ArrowRight":
-            keys.right = false
             player.isMoving = false
+            keys.right = false
+            console.log('fuck');
+
+            player.image = player.sprites.stand
+            break
         case "UpArrow":
             keys.up = false
         case "DownArrow":
