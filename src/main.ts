42960e777474ec2c562b174587e59d022e6dbb60
yoav <yoavdan.m@gmail.com>
2022-09-13 17:54:12 +0300

    pls work

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import { playerStand } from './ts/character'
 import { Sprite, Player } from './ts/classes'
 import { portals, } from './ts/portals'
 
-export const canvas = document.getElementById('canvas') as HTMLCanvasElement
+const bgImage = new Image()
+bgImage.src = "/src/assets/game-map.png"
 
+const canvas = document.getElementById('canvas') as HTMLCanvasElement
 export const c = canvas.getContext('2d')
 
 
@@ -14,8 +16,6 @@ canvas.height = window.innerWidth > 600 ? 680 : 480
 c!.fillStyle = 'white'
 c?.fillRect(0, 0, canvas.width, canvas.height)
 
-const bgImage = new Image()
-bgImage.src = "/src/assets/game-map.png"
 
 
 const background = new Sprite({ x: -100, y: -520 }, bgImage)
